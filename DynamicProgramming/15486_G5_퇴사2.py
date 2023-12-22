c6f259a693bf20fce94a9fc5da02d56cadef3539
potential1205 <dlwogns1205@gmail.com>
2023-12-22 17:13:06 +0900

    2023-12-22

--- DynamicProgramming/15486_G5_퇴사2.py
+++ DynamicProgramming/15486_G5_퇴사2.py
@@ -4,13 +4,12 @@ input = sys.stdin.readline
 
 if __name__ == "__main__":
     n = int(input())
-    lst = [0] + [list(map(int,input().split())) for i in range(n)]
     dp = [0] * (n+1)
 
-    for i in range(1,n+1):
-        dp[i] = max(dp[i], dp[i-1])
-        day,profit = lst[i]
-        if i+day-1 <= n:
-            dp[i+day-1] = max(dp[i+day-1], dp[i-1]+profit)
+    for day in range(1,n+1):
+        t,p = map(int,input().split())
+        dp[day] = max(dp[day], dp[day-1])
+        if day + t -1 <= n:
+            dp[day+t-1] = max(dp[day+t-1], dp[day-1]+p)
     
     print(dp[-1])
