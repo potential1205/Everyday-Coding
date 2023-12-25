2fd944dc8a57df5b17cb37715ebe74ea919283f8
potential1205 <dlwogns1205@naver.com>
2023-12-25 13:22:56 +0900

    2023-12-25

--- Implementation/20055_G5_컨베이어벨트위의로봇.py
+++ Implementation/20055_G5_컨베이어벨트위의로봇.py
@@ -1,35 +1,35 @@
 from collections import deque
 
-def rotate():
-    # 벨트 회전
-    temp = belt[2*n-1]
-    for i in range(2*n-1,0,-1):
-        belt[i] = belt[i-1]
-    belt[0] = temp
+# def rotate():
+#     # 벨트 회전
+#     temp = belt[2*n-1]
+#     for i in range(2*n-1,0,-1):
+#         belt[i] = belt[i-1]
+#     belt[0] = temp
 
-    # 벨트 회전에 의한 로봇 회전
-    for i in range(len(robot)):
-        robot[i] += 1
+#     # 벨트 회전에 의한 로봇 회전
+#     for i in range(len(robot)):
+#         robot[i] += 1
     
-    if len(robot)!=0 and robot[0] == 2*n-1:
-        robot.popleft()
+#     if len(robot)!=0 and robot[0] == 2*n-1:
+#         robot.popleft()
 
-    return
+#     return
 
 
-def move():
-    flag = False
-    for i in range(len(robot)):
-        if robot[i] + 1 == 2*n-1: # 만약 로봇의 다음 칸이 내리는 위치라면
-            flag = True
-        else:
-            if (robot[i]+1) not in robot and belt[robot[i]+1] > 0:
-                robot[i] += 1
+# def move():
+#     flag = False
+#     for i in range(len(robot)):
+#         if robot[i] + 1 == 2*n-1: # 만약 로봇의 다음 칸이 내리는 위치라면
+#             flag = True
+#         else:
+#             if (robot[i]+1) not in robot and belt[robot[i]+1] > 0:
+#                 robot[i] += 1
     
-    if flag:
-        robot.popleft()
+#     if flag:
+#         robot.popleft()
 
-    return
+#     return
 
 
 if __name__ == "__main__":
@@ -38,8 +38,6 @@ if __name__ == "__main__":
     t = 0
 
     while True:
-        t += 1
-
         if belt.count(0) >= k:
             break
         
@@ -57,7 +55,9 @@ if __name__ == "__main__":
         if robot[0] == 0 and belt[0] > 0:
             robot[0] = 1
             belt[0] -= 1
+        
+        t += 1
     
-    print(t-1)
+    print(t)
  
         
