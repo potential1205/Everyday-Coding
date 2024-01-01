107d764827c1f5f44bf88964d84578cd258358c5
potential1205 <dlwogns1205@naver.com>
2024-01-01 22:04:32 +0900

    2023-01-01

--- Graph/2668_G5_숫자고르기.py
+++ Graph/2668_G5_숫자고르기.py
@@ -1,32 +1,34 @@
 import sys
 input = sys.stdin.readline
+from collections import defaultdict
+
+def dfs(node,history):
+    history.append(node)
+    visit[node] = True
+
+    for v in graph[node]:
+        if v not in history:
+            dfs(v,history[:])
+        else:
+            result.extend(history)
+            return
 
 if __name__ == "__main__":
-    n = int(input())
-    lst1 = [i for i in range(1,n+1)]
-    lst2 = [int(input()) for i in range(n)]
-    
-    board = [[0 for i in range(n)] for j in range(n)]
+    n = int(sys.stdin.readline().strip())
+    graph = defaultdict(list)
+    for i in range(1, n+1):
+        v = int(int(input()))
+        graph[v].append(i)
 
-    for i in range(n):
-        board[lst1[i]-1][lst2[i]-1] = 1
-    
-    
-    print("hello")
-    for i in range(2):
-        end = i
-        t,f = 0,0
-        answer = []
-        while True:
-            if board[t][f] == 1:
-                t = f
-                f = 0
-                answer.append(t)
-                if t == end:
-                    print(answer)
-                    break
-            else:
-                f+=1
-                if f == n:
-                    break
+    print(graph)
 
+    visit = [False for i in range(n+1)]
+    result = []
+    for i in range(1,n+1):
+        if not visit[i]:
+            dfs(i,[])
+
+    result.sort()
+    print(len(result))
+    for val in result:
+        print(val)
