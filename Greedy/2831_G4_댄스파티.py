fbbbc80f1d731fa18eb4194df0b69e409572d4cd
potential1205 <dlwogns1205@gmail.com>
2024-01-02 20:02:42 +0900

    2024-01-02

--- Greedy/2831_G4_댄스파티.py
+++ Greedy/2831_G4_댄스파티.py
@@ -1,67 +1,43 @@
+
+def solution(lst1,lst2):
+    global cnt
+    idx,jdx = 0,0
+    leng1,leng2 = len(lst1), len(lst2)
+
+    lst1.sort(reverse=True)
+    lst2.sort()
+
+    while True:
+        if idx == leng1 or jdx == leng2:
+            break
+        
+        if abs(lst1[idx]) > abs(lst2[jdx]):
+            cnt+=1
+            idx+=1
+            jdx+=1
+        else:
+            idx+=1
+    return
+
 if __name__ == "__main__":
     n = int(input())
+
+
     men = list(map(int,input().split()))
     women = list(map(int,input().split()))
 
-    left_men = []
-    right_men = []
-
-    left_women = []
-    right_women = []
+    left_men,right_men = [],[]
+    left_women,right_women = [],[]
 
     for man in men:
-        if man < 0:
-            left_men.append(man)
-        else:
-            right_men.append(man)
+        if man < 0: left_men.append(man)
+        else: right_men.append(man)
     
     for woman in women:
-        if woman > 0:
-            left_women.append(woman)
-        else:
-            right_women.append(woman)
+        if woman > 0: left_women.append(woman)
+        else: right_women.append(woman)
     
-    idx = 0
-    jdx = 0
-    
-    mlen = len(left_men)
-    wlen = len(left_women)
-
-    left_men.sort(reverse=True)
-    left_women.sort()
-
     cnt = 0
-
-    while True:
-        if idx == mlen or jdx == wlen:
-            break
-        
-        if abs(left_men[idx]) > abs(left_women[jdx]):
-            cnt+=1
-            idx+=1
-            jdx+=1
-        else:
-            idx+=1
-
-
-    idx = 0
-    jdx = 0
-    
-    mlen = len(right_men)
-    wlen = len(right_women)
-
-    right_men.sort()
-    right_women.sort(reverse=True)
-
-    while True:
-        if idx == mlen or jdx == wlen:
-            break
-        
-        if abs(right_men[idx]) < abs(right_women[jdx]):
-            cnt+=1
-            idx+=1
-            jdx+=1
-        else:
-            jdx+=1
-    
+    solution(left_men,left_women)
+    solution(right_women,right_men)    
     print(cnt)
