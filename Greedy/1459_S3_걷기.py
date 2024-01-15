75ca4d4a3c6aa60c9e7d589a3201f09e3030ff72
potential1205 <dlwogns1205@naver.com>
2024-01-15 09:52:18 +0900

    2024-01-15

--- Greedy/1459_S3_걷기.py
+++ Greedy/1459_S3_걷기.py
@@ -2,25 +2,16 @@
 
 if __name__ == "__main__":
     m,n,c1,c2 = map(int,input().split())
+
     answer = 0
+    big, small = max(m,n), min(m,n)
 
-    rest_val = m + n
-    max_val = max(m,n)
-    min_val = min(m,n)
-
-    if (rest_val)%2!=0:
-        max_val -= 1
+    if (big + small)%2!=0:
+        big -= 1
         answer += c1
 
-    if 2*c1 >= c2:
-        answer += (min_val*c2)
-        max_val = max_val - min_val
-        min_val = 0
-    else:
-        answer += (min_val*c1*2)
-        max_val = max_val - min_val
-        min_val = 0
-
+    answer += (small * min(c1*2, c2))
+    max_val = big - small
     answer += (max_val * min(c1, c2))
     
     print(answer)
