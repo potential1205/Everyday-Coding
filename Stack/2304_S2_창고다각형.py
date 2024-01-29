d52eef071501e0ab71f22d722f07cc8ea639d96b
potential1205 <potential1205@naver.com>
2024-01-29 14:08:47 +0900

    2024-01-29

--- Stack/2304_S2_창고다각형.py
+++ Stack/2304_S2_창고다각형.py
@@ -1,61 +1,24 @@
+import sys
+input = sys.stdin.readline
 
-from collections import deque
+max_h = 0
+max_x = 0
+pli = [0 for _ in range(1001)]
+for _ in range(int(input())):
+    x,h = map(int,input().split())
+    pli[x] = h
+    if max_h < h:
+        max_x = x
+        max_h = h
 
-if __name__ == "__main__":
-    n = int(input())
-    x = []
-    y = []
-    for i in range(n):
-        a,b = map(int,input().split())
-        x.append((a,b))
-        y.append((b,a))
+curr = 0
+answer = 0
+for i in range(max_x+1):
+    curr = max(curr,pli[i])
+    answer += curr
+curr = 0
+for i in range(1000,max_x,-1):
+    curr = max(curr,pli[i])
+    answer += curr
 
-    x.sort()
-    y.sort()
-
-    max_h = y[-1][0]
-    stack = deque([[x[0][0],x[0][1]]])
-
-    for i in range(1,n):
-        kx,ky = x[i]
-        if ky == max_h:
-            stack.append([kx,ky])
-
-            idx = i+1
-            break
-
-        if stack[-1][1] < ky:
-            stack.append([kx,ky])
-        else:
-            continue
-    
-    stack2 = deque([[x[idx][0],x[idx][1]]])
-
-    for i in range(idx+1,n):
-        kx,ky = x[i]
-
-        if stack2[-1][1] < ky:
-            while stack2:
-                stack2.popleft()
-            stack2.append([kx,ky])
-        else:
-            stack2.append([kx,ky])
-
-    answer = 0
-
-    for i in range(len(stack)-1):
-        gap = stack[i+1][0] - stack[i][0]
-        answer = answer + gap*stack[i][1]
-    
-    answer += max_h
-
-    a,b = stack2.popleft()
-    answer += (a-stack[i+1][0])*b
-
-    for i in range(len(stack2)-1):
-        gap = stack2[i+1][0] - stack2[i][0]
-        answer = answer + gap*stack2[i+1][1]
-    
-
-    print(answer)
-
+print(answer)
