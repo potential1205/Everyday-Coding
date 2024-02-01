9a2bb1b4fb4ab0b42e3ee7de9a306be0d9026cb8
potential1205 <potential1205@naver.com>
2024-02-01 14:37:14 +0900

    2024-02-01

--- Implementation/23288_G3_주사위굴리기2.py
+++ Implementation/23288_G3_주사위굴리기2.py
@@ -1,41 +1,28 @@
 from collections import deque
-dy = [-1,1,0,0]
-dx = [0,0,-1,1]
+dy = [0,1,0,-1]
+dx = [1,0,-1,0]
+
+def get_direction(dice):
+    a = dice["state"][5]
+    b = board[dice["y"]][dice["x"]]
+
+    if a>b:
+        dice["d"] = (dice["d"] + 1)%4
+    elif b>a:
+        dice["d"] = (dice["d"] - 1)%4
+
+    return dice
 
 def rotate_d(d):
     if d == 0:
-        d = 1
-    elif d == 1:
-        d = 0
-    elif d == 2:
-        d = 3
-    elif d == 3:
         d = 2
+    elif d == 1:
+        d = 3
+    elif d == 2:
+        d = 0
+    elif d == 3:
+        d = 1
     return d
-
-def roll(dice,d):
-    lst = dice["state"]
-    if d == 0: # 북쪽으로 굴러
-        lst[0],lst[1],lst[2],lst[3],lst[4],lst[5] = lst[4],lst[0],lst[2],lst[3],lst[5],lst[1]
-    elif d == 1: # 남쪽으로 굴러
-        lst[0],lst[1],lst[2],lst[3],lst[4],lst[5] = lst[1],lst[5],lst[2],lst[3],lst[0],lst[4]
-    elif d == 2: # 서쪽으로 굴러
-        lst[0],lst[1],lst[2],lst[3],lst[4],lst[5] = lst[2],lst[1],lst[5],lst[0],lst[4],lst[3]
-    elif d == 3: # 동쪽으로 굴러
-        lst[0],lst[1],lst[2],lst[3],lst[4],lst[5] = lst[3],lst[1],lst[0],lst[5],lst[4],lst[2]
-    return dice
-
-def move(dice):
-    ky,kx = dice["y"]+dy[dice["d"]], dice["x"]+dx[dice["d"]]
-    if ky<0 or ky>=n or kx<0 or kx>=m:
-        dice["d"] = rotate_d(dice["d"])
-        ky,kx = dice["y"]+dy[dice["d"]], dice["x"]+dx[dice["d"]]
-
-    dice = roll(dice,dice["d"])
-    dice["y"] = ky
-    dice["x"] = kx
-
-    return dice
 
 def bfs(dice):
     queue = deque([[dice["y"],dice["x"]]])
@@ -59,33 +46,31 @@ def bfs(dice):
 
     return cnt
 
-
 def get_score(dice):
     c = bfs(dice)
     return c * board[dice["y"]][dice["x"]]
 
-def get_direction(dice):
-    a = dice["state"][5]
-    b = board[dice["y"]][dice["x"]]
+def roll(dice,d):
+    lst = dice["state"]
+    if d == 0:
+        lst[0],lst[1],lst[2],lst[3],lst[4],lst[5] = lst[3],lst[1],lst[0],lst[5],lst[4],lst[2]
+    elif d == 1:
+        lst[0],lst[1],lst[2],lst[3],lst[4],lst[5] = lst[1],lst[5],lst[2],lst[3],lst[0],lst[4]
+    elif d == 2:
+        lst[0],lst[1],lst[2],lst[3],lst[4],lst[5] = lst[2],lst[1],lst[5],lst[0],lst[4],lst[3]
+    elif d == 3:
+        lst[0],lst[1],lst[2],lst[3],lst[4],lst[5] = lst[4],lst[0],lst[2],lst[3],lst[5],lst[1]
+    return dice
 
-    if a>b:
-        if dice["d"] == 0:
-            dice["d"] = 3
-        elif dice["d"] == 1:
-            dice["d"] = 2
-        elif dice["d"] == 2:
-            dice["d"] = 0
-        elif dice["d"] == 3:
-            dice["d"] = 1
-    elif b>a:
-        if dice["d"] == 0:
-            dice["d"] = 2
-        elif dice["d"] == 1:
-            dice["d"] = 3
-        elif dice["d"] == 2:
-            dice["d"] = 1
-        elif dice["d"] == 3:
-            dice["d"] = 0
+def move(dice):
+    ky,kx = dice["y"]+dy[dice["d"]], dice["x"]+dx[dice["d"]]
+    if ky<0 or ky>=n or kx<0 or kx>=m:
+        dice["d"] = rotate_d(dice["d"])
+        ky,kx = dice["y"]+dy[dice["d"]], dice["x"]+dx[dice["d"]]
+
+    dice = roll(dice,dice["d"])
+    dice["y"] = ky
+    dice["x"] = kx
 
     return dice
 
@@ -98,9 +83,7 @@ if __name__ == "__main__":
     
     dice = dict()
     dice["state"] = [1,2,3,4,5,6] # 천장, 상, 우, 좌, 하, 바닥
-    dice["d"] = 3 # 0위 1아래 2왼쪽 3오른쪽
-    dice["y"] = 0
-    dice["x"] = 0
+    dice["d"], dice["y"], dice["x"] = 0, 0, 0
 
     answer = 0
     for turn in range(k):
