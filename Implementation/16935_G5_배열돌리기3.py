2884807fe743066cd7ec21f69a483c5405db32b7
potential1205 <dlwogns1205@naver.com>
2024-02-16 21:40:37 +0900

    2024-02-16

--- Implementation/16935_G5_배열돌리기3.py
+++ Implementation/16935_G5_배열돌리기3.py
@@ -1,32 +1,25 @@
 
-def f1(board):
-    global n,m
+def f1(boardn,n,m):
     for i in range(m):
         for j in range(n//2):
             board[j][i], board[n-j-1][i] = board[n-j-1][i], board[j][i]
     return board
 
-def f2(board):
-    global n,m
+def f2(board,n,m):
     for i in range(n):
         for j in range(m//2):
             board[i][j], board[i][m-j-1] = board[i][m-j-1], board[i][j]
     return board
 
-def f3(board):
-    global n,m
-    board = list(map(list, zip(*board[::-1])))
+def f3(board,n,m):
     n,m = m,n
-    return board
+    return list(map(list,zip(*board[::-1]))),n,m
 
-def f4(board):
-    global n,m
-    board = list(map(list, zip(*board)))[::-1]
+def f4(board,n,m):
     n,m = m,n
-    return board
+    return list(map(list,zip(*board)))[::-1],n,m
 
-def f5(board):
-    global n,m
+def f5(board,n,m):
     for i in range(n//2):
         for j in range(m//2):
             temp = board[i][j]
@@ -36,8 +29,7 @@ def f5(board):
             board[i][j+m//2] = temp
     return board
 
-def f6(board):
-    global n,m
+def f6(board,n,m):
     for i in range(n//2):
         for j in range(m//2):
             temp = board[i][j]
@@ -45,7 +37,6 @@ def f6(board):
             board[i][j+m//2] = board[i+n//2][j+m//2]
             board[i+n//2][j+m//2] = board[i+n//2][j]
             board[i+n//2][j] = temp
-
     return board
 
 if __name__ == "__main__":
@@ -58,17 +49,17 @@ if __name__ == "__main__":
     
     for op in ops:
         if op == 1:
-            board = f1(board)
+            board = f1(board,n,m)
         elif op == 2:
-            board = f2(board)
+            board = f2(board,n,m)
         elif op == 3:
-            board = f3(board)
+            board,n,m = f3(board,n,m)
         elif op == 4:
-            board = f4(board)
+            board,n,m = f4(board,n,m)
         elif op == 5:
-            board = f5(board)
+            board = f5(board,n,m)
         elif op == 6:
-            board = f6(board)
+            board = f6(board,n,m)
 
-    for i in range(len(board)):
+    for i in range(n):
         print(*board[i])
