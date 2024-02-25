ff1a176081e7907a2c5ff21ec3fbb498f324096b
potential1205 <dlwogns1205@naver.com>
2024-02-25 23:53:34 +0900

    2024-02-25

--- Sorting/2170_G5_선긋기.py
+++ Sorting/2170_G5_선긋기.py
@@ -5,21 +5,25 @@ if __name__ == "__main__":
     n = int(input())
     lines = [list(map(int,input().split())) for i in range(n)]
 
-    lines.sort()
+    lines.sort(key = lambda x : (x[0],-x[1]))
     answer = [lines[0][0],lines[0][1]]
-
+    result = []
+    
     for i in range(1,len(lines)):
-        a,b = answer[0],answer[1]
-        flag1 = False
+        start,end = lines[i]
 
-        if b>=lines[i][0] and b<lines[i][1]:
-            flag1 = True
-        
-        if lines[i][0] < a and lines[i][1] >=a:
-            answer[0] = lines[i][0]
+        if start == answer[0]:
+            continue
 
-        if flag1 == True:
-            answer[1] = lines[i][1]
+        if start <= answer[1]:
+            if end > answer[1]:
+                answer[1] = end
+        else:
+            result.append(answer[1]-answer[0])
+            answer[0], answer[1] = start,end
+    
+    result.append(answer[1]-answer[0])
+    
 
-
-    print(answer[1]-answer[0]+1)
+    print(result)
+    print(sum(result))
