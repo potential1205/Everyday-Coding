dd09236e0324c1298dfd08d7fd6156b58cbed83c
potential1205 <potential1205@naver.com>
2024-03-01 17:46:57 +0900

    2024-03-01

--- Implementation/17142_G3_연구소3.py
+++ Implementation/17142_G3_연구소3.py
@@ -1,93 +1,66 @@
 from collections import deque
 from itertools import combinations
+import sys
+INF = sys.maxsize
 
 dy = [-1,1,0,0]
 dx = [0,0,-1,1]
 
-def bfs(board,candidate):
-    #print(candidate)
-    visit = [[0 for j in range(n)] for i in range(n)]
-
-    for i in range(n):
-        for j in range(n):
-            if board[i][j] == 1:
-                board[i][j] = '-'
-                visit[i][j] = -1
-            elif board[i][j] == 2:
-                board[i][j] = '*'
-
+def bfs(board,candidate,zero_cnt):
+    visit = [[False for j in range(n)] for i in range(n)]
     queue = deque([])
     for i in range(m):
         y,x = candidate[i]
-        queue.append([y,x])
-        board[y][x] = 'O'
-        visit[y][x] = 0
+        queue.append((y,x))
+        visit[y][x] = True
     
-
+    time = 0
     while queue:
-        y,x = queue.popleft()
-        for i in range(4):
-            ky,kx = y+dy[i],x+dx[i]
+        if zero_cnt == 0:
+            return time
 
-            if ky<0 or kx<0 or ky>=n or kx>=n or visit[ky][kx]!=0 or board[ky][kx] == '-' or board[ky][kx]=='O':
-                continue
-            
-            if board[ky][kx] == '*':
-                board[ky][kx] = 'O'
-                visit[ky][kx] = visit[y][x]
-                queue.append([ky,kx])
-            else:
-                queue.append([ky,kx])
-                visit[ky][kx] = visit[y][x] + 1
-    
-    # for i in range(n):
-    #     print(*board[i])
-    # print()
+        time += 1
 
-    # for i in range(n):
-    #     print(*visit[i])
-    # print()
+        for i in range(len(queue)):
+            y,x = queue.popleft()
 
-    cnt=0
-    a = 0
-    for i in range(n):
-        for j in range(n):
-            if visit[i][j] == 0 and board[i][j] == 0:
-                cnt+=1
-            a = max(a,visit[i][j])
-    
-    print(cnt,a)
-    if cnt==0:
-       return a
-    else:
-        return -1 
+            for j in range(4):
+                ky,kx = y+dy[j], x+dx[j]
+
+                if ky<0 or kx<0 or ky>=n or kx>=n or visit[ky][kx] == True:
+                    continue
+
+                if board[ky][kx] == 0:
+                    visit[ky][kx] = True
+                    queue.append((ky,kx))
+                    zero_cnt-=1
+                
+                elif board[ky][kx] == 2:
+                    visit[ky][kx] = True
+                    queue.append((ky,kx))
+    return INF
 
 if __name__ == "__main__":
     n,m = map(int,input().split())
-    board = []
-    virus = []
+    board, virus = [list(map(int,input().split())) for i in range(n)], []
+
+    zero_cnt = 0
     for i in range(n):
-        line = list(map(int,input().split()))
-
-        for j in range(len(line)):
-            if line[j] == 2:
+        for j in range(n):
+            if board[i][j] == 0:
+                zero_cnt+=1
+            if board[i][j] == 2:
                 virus.append([i,j])
-        board.append(line)
 
-    candidate = list(combinations(virus,m))
-
-    answer = 99999
-    flag = False
-    for i in range(len(candidate)):
-        val = bfs(board,candidate[i])
-        if val != -1:
-            answer = min(answer,val)
-            flag= True
-
-    if flag:
-        print(answer)
-    else:
-        print(-1)
-
+    answer = INF
+    for comb in list(combinations(virus,m)):
+        res = bfs(board,comb,zero_cnt)
+        answer = min(answer,res)
     
+    if answer == INF:
+        print(-1)
+    else:
+        print(answer)
+
+
 
