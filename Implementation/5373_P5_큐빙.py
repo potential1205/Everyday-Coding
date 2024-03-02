9f9683ca2b59b508c4756ee20ec3dc4a7ed22398
potential1205 <dlwogns1205@naver.com>
2024-03-02 16:55:50 +0900

    2024-03-02

--- Implementation/5373_P5_큐빙.py
+++ Implementation/5373_P5_큐빙.py
@@ -35,14 +35,15 @@ if __name__ == "__main__":
                     cube[2][0][0],cube[2][0][1],cube[2][0][2] = cube[5][0][0],cube[5][0][1],cube[5][0][2]
                     cube[5][0][0],cube[5][0][1],cube[5][0][2] = cube[1][0][0],cube[1][0][1],cube[1][0][2]
                     cube[1][0][0],cube[1][0][1],cube[1][0][2] = temp1,temp2,temp3
+                    cube[4] = list(map(list,zip(*cube[4][::-1])))
                 elif b == '-':
                     temp1,temp2,temp3 = cube[0][0][0],cube[0][0][1],cube[0][0][2]
                     cube[0][0][0],cube[0][0][1],cube[0][0][2] = cube[1][0][0],cube[1][0][1],cube[1][0][2]
                     cube[1][0][0],cube[1][0][1],cube[1][0][2] = cube[5][0][0],cube[5][0][1],cube[5][0][2]
                     cube[5][0][0],cube[5][0][1],cube[5][0][2] = cube[2][0][0],cube[2][0][1],cube[2][0][2]
                     cube[2][0][0],cube[2][0][1],cube[2][0][2] = temp1,temp2,temp3
+                    cube[4] = list(map(list,zip(*cube[4])))[::-1]
 
-    
             elif a == 'D':
                 if b == '+':
                     temp1,temp2,temp3 = cube[0][2][0],cube[0][2][1],cube[0][2][2]
@@ -50,19 +51,53 @@ if __name__ == "__main__":
                     cube[2][2][0],cube[2][2][1],cube[2][2][2] = cube[5][2][0],cube[5][2][1],cube[5][2][2]
                     cube[5][2][0],cube[5][2][1],cube[5][2][2] = cube[1][2][0],cube[1][2][1],cube[1][2][2]
                     cube[1][2][0],cube[1][2][1],cube[1][2][2] = temp1,temp2,temp3
+                    cube[3] = list(map(list,zip(*cube[3][::-1])))
                 elif b == '-':
                     temp1,temp2,temp3 = cube[0][2][0],cube[0][2][1],cube[0][2][2]
                     cube[0][2][0],cube[0][2][1],cube[0][2][2] = cube[1][2][0],cube[1][2][1],cube[1][2][2]
                     cube[1][2][0],cube[1][2][1],cube[1][2][2] = cube[5][2][0],cube[5][2][1],cube[5][2][2]
                     cube[5][2][0],cube[5][2][1],cube[5][2][2] = cube[2][2][0],cube[2][2][1],cube[2][2][2]
                     cube[2][2][0],cube[2][2][1],cube[2][2][2] = temp1,temp2,temp3
+                    cube[3] = list(map(list,zip(*cube[3])))[::-1]
             
-            # elif a == 'F':
+            elif a == 'F':
+                if b == '+':
+                    temp1,temp2,temp3 = cube[2][0][0],cube[2][1][0],cube[2][2][0]
+                    cube[2][0][0],cube[2][1][0],cube[2][2][0] = cube[4][2][0],cube[4][2][1],cube[4][2][2]
+                    cube[4][2][0],cube[4][2][1],cube[4][2][2] = cube[1][2][2],cube[1][1][2],cube[1][0][2]
+                    cube[1][2][2],cube[1][1][2],cube[1][0][2] = cube[3][0][2],cube[1][0][1],cube[3][0][0]
+                    cube[3][0][2],cube[1][0][1],cube[3][0][0] = temp1,temp2,temp3
+                    cube[0] = list(map(list,zip(*cube[0][::-1])))
+
+                elif b == '-':
+                    temp1,temp2,temp3 = cube[2][0][0],cube[2][1][0],cube[2][2][0]
+                    cube[2][0][0],cube[2][1][0],cube[2][2][0] = cube[3][0][2],cube[3][0][1],cube[3][0][0]
+                    cube[3][0][0],cube[3][0][1],cube[3][0][2] = cube[1][0][2],cube[1][1][2],cube[1][2][2]
+                    cube[1][0][2],cube[1][1][2],cube[1][2][2] = cube[4][2][2],cube[4][2][1],cube[4][2][0]
+                    cube[4][2][0],cube[4][2][1],cube[4][2][2] = temp1,temp2,temp3 
+                    cube[0] = list(map(list,zip(*cube[0])))[::-1]
             
-            # elif a == 'B':
+            elif a == 'B':
+                if b == '+':
+                    temp1,temp2,temp3 = cube[2][0][2],cube[2][1][2],cube[2][2][2]
+                    cube[2][0][2],cube[2][1][2],cube[2][2][2] = cube[3][0][2],cube[3][1][2],cube[3][2][2]
+
+                    cube[2][0][0],cube[2][1][0],cube[2][2][0] = cube[4][2][0],cube[4][2][1],cube[4][2][2]
+                    cube[4][2][0],cube[4][2][1],cube[4][2][2] = cube[1][2][2],cube[1][1][2],cube[1][0][2]
+                    cube[1][2][2],cube[1][1][2],cube[1][0][2] = cube[3][0][2],cube[1][0][1],cube[3][0][0]
+                    cube[3][0][2],cube[1][0][1],cube[3][0][0] = temp1,temp2,temp3
+                    cube[0] = list(map(list,zip(*cube[5][::-1])))
+
+                elif b == '-':
+                    temp1,temp2,temp3 = cube[2][0][0],cube[2][1][0],cube[2][2][0]
+                    cube[2][0][0],cube[2][1][0],cube[2][2][0] = cube[3][0][2],cube[3][0][1],cube[3][0][0]
+                    cube[3][0][0],cube[3][0][1],cube[3][0][2] = cube[1][0][2],cube[1][1][2],cube[1][2][2]
+                    cube[1][0][2],cube[1][1][2],cube[1][2][2] = cube[4][2][2],cube[4][2][1],cube[4][2][0]
+                    cube[4][2][0],cube[4][2][1],cube[4][2][2] = temp1,temp2,temp3 
+                    cube[0] = list(map(list,zip(*cube[5])))[::-1]
             
             # elif a == 'L':
             
             # elif a == 'R':
 
- 
+        print(cube)
